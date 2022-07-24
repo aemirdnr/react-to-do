53d8f361a303040a25e2d3fc66fead6607026640
aemirdnr <aemirdnr@gmail.com>
2022-07-25 02:47:15 +0300

    + removeTask() function

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -4,32 +4,33 @@ import { useState } from "react";
 
 function Task(task) {
   const [isFinish, setFinish] = useState(task.isDone);
+  const [isDeleted, setDelete] = useState(false);
 
   const finishTask = () => {
     setFinish(true);
   };
 
-  const removeTask = () => {};
+  const removeTask = () => {
+    setDelete(true);
+  };
 
-  return (
-    <>
-      {
-        <div
-          className={
-            isFinish
-              ? "task task-done w-75 mx-auto my-3 p-3 gap-3"
-              : "task w-75 mx-auto my-3 p-3 gap-3"
-          }
-        >
-          <p onClick={finishTask} className="task__text m-0">
-            {task.text}
-          </p>
-          <i onClick={removeTask} className="task__icon">
-            <FaTrash />
-          </i>
-        </div>
+  return !isDeleted ? (
+    <div
+      className={
+        isFinish
+          ? "task task-done w-75 mx-auto my-3 p-3 gap-3"
+          : "task w-75 mx-auto my-3 p-3 gap-3"
       }
-    </>
+    >
+      <p onClick={finishTask} className="task__text m-0">
+        {task.text}
+      </p>
+      <i onClick={removeTask} className="task__icon">
+        <FaTrash />
+      </i>
+    </div>
+  ) : (
+    ""
   );
 }
 
